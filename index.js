4af01945f09857915ceb79e1883ec9d167ecd962
Naomi Basu <naomibasu@gmail.com>
2024-07-01 18:08:08 -0400

    Add Bhavacakra remix

--- index.js
+++ index.js
@@ -6,65 +6,72 @@ const artDirectory = "images/paintings/";
 const thumbnailDirectory = "images/thumbnails/";
 var art_list = [
   {
+    filename: 'Naomi_Basu_BhavacakraRemix_2024.jpeg',
+    title: 'Bhavacakra Remix',
+    medium: 'Acrylic, airbrush, indigo, crayon, sumi ink, sequins, ash, gold leaf on canvas',
+    dimensions: '30 x 40 inches',
+    year: '2024'
+  },
+  {
     filename: 'Naomi_Basu_Bushido_2023.jpeg',
     title: 'Bushido',
-    medium: 'Acrylic, Mineral Pigments, and Resin on Wood',
+    medium: 'Acrylic, mineral pigments, and resin on wood',
     dimensions: '12 x 12 inches',
     year: '2023'
   },
   {
     filename: 'Naomi_Basu_Vajra_2023.jpeg',
     title: 'Vajra',
-    medium: 'Acrylic, Mineral Pigments, and Resin on Wood',
+    medium: 'Acrylic, mineral pigments, and resin on wood',
     dimensions: '12 x 12 inches',
     year: '2023'
   },
   {
     filename: 'Naomi_Basu_VapeNation_2023.jpeg',
     title: 'VapeNation',
-    medium: 'Acrylic, Mineral Pigments, and Resin on Wood',
+    medium: 'Acrylic, mineral pigments, and resin on wood',
     dimensions: '11.5 x 11.5 inches',
     year: '2023'
   },
   {
     filename: 'Naomi_Basu_EnglishBreakfast_2023.jpeg',
     title: 'English Breakfast',
-    medium: 'Acrylic, Indigo, Tea, Gold Leaf, and Resin on Wood',
+    medium: 'Acrylic, indigo, tea, gold Leaf, and resin on wood',
     dimensions: '11.5 x 11.5 inches',
     year: '2023'
   },
   {
     filename: 'Naomi_Basu_Metamorphose_2023.jpeg',
     title: 'Metamorphose',
-    medium: 'Acrylic, Ground Indigo, and Resin on Wood',
+    medium: 'Acrylic, ground indigo, and resin on wood',
     dimensions: '28 x 28 inches',
     year: '2023'
   },
   {
     filename: 'Naomi_Basu_Milk_Bath_2023.jpeg',
     title: 'Milk Bath',
-    medium: 'Oil and Muslin on Unstretched Canvas',
+    medium: 'Oil and muslin on unstretched canvas',
     dimensions: '36 x 42 inches',
     year: '2023'
   },
   {
     filename: 'Naomi_Basu_BornInALakeOfTears_2022.jpeg',
     title: 'Born in a Lake of Tears',
-    medium: 'Acrylic and Recycled Sari on Unstretched Canvas',
+    medium: 'Acrylic and recycled sari on unstretched canvas',
     dimensions: '37.5 x 42 inches',
     year: '2022'
   },
   {
     filename: 'Naomi_Basu_SkyDancer_2022.jpeg',
     title: 'Sky Dancer',
-    medium: 'Acrylic on Canvas',
+    medium: 'Acrylic on canvas',
     dimensions: '28 x 28 inches',
     year: '2022'
   },
   {
     filename: 'Naomi_Basu_DawonI_2021.jpeg',
     title: 'Dawon (I)',
-    medium: 'Acrylic and Resin on Wood',
+    medium: 'Acrylic and resin on wood',
     dimensions: '17 x 17 inches',
     year: '2021'
   },
@@ -72,14 +79,14 @@ var art_list = [
     filename: 'Naomi_Basu_DawonII_2021.jpeg',
     minImage: 'Naomi_Basu_DawonII_2021.jpeg',
     title: 'Dawon (II)',
-    medium: 'Acrylic and Resin on Wood',
+    medium: 'Acrylic and resin on wood',
     dimensions: '17 x 17 inches',
     year: '2021'
   },
   {
     filename: 'Naomi_Basu_Nayikows_2021.jpeg',
     title: 'Nayikows',
-    medium: 'Embroidery on Raw Silk Sari',
+    medium: 'Embroidery on raw silk sari',
     dimensions: '9 x 26 inches',
     year: '2021'
   },
